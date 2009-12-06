ef507b7e9c5ca0fc57adc6f4553130809f6e511c
Gregor Best <gbe@intepi.net>
2009-12-06 20:35:07 +0100

    minor code fix
    Signed-off-by: Gregor Best <gbe@intepi.net>

--- src/decorate.h
+++ src/decorate.h
@@ -1,15 +1,15 @@
 /* decorate.h
-	definitions for parsing decorate files
+    definitions for parsing decorate files
 */
 
 #ifndef YH_DECORATE
 #define YH_DECORATE
 
-#define STATE "spawn" 	// state to read the first graphic from
-#define STATELEN 4	// length of the state name
-#define SPRITELEN 4   	// length of the sprites name
+#define STATE "spawn" // state to read the first graphic from
+#define STATELEN 4    // length of the state name
+#define SPRITELEN 4   // length of the sprites name
 #define MAX_TOKENS 6
 
 void SaveEntryToRawFile (FILE *, const char *);
-
+void read_decorate();
 #endif

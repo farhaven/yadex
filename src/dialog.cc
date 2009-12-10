2cd785ba8aaa00045ac3cb3adb00309a32cf3f85
Gregor Best <gbe@intepi.net>
2009-12-11 00:01:56 +0100

    code cleanup
    Signed-off-by: Gregor Best <gbe@intepi.net>

--- src/dialog.cc
+++ src/dialog.cc
@@ -4,7 +4,6 @@
  *	AYM 1998-11-30
  */
 
-
 /*
 This file is part of Yadex.
 
@@ -27,12 +26,10 @@ this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 Place, Suite 330, Boston, MA 02111-1307, USA.
 */
 
-
 #include "yadex.h"
 #include <X11/Xlib.h>
 #include "dialog.h"
 #include "gfx.h"
-
 
 /*
  *	Confirm - ask for confirmation
@@ -43,65 +40,71 @@ Place, Suite 330, Boston, MA 02111-1307, USA.
  */
 bool Confirm (int x0, int y0, const char *prompt1, const char *prompt2)
 {
-  const char *const prompt3 = "Press [Y] to confirm, [N] to cancel...";
-  size_t maxlen;
-  double n_lines_of_text;
-  int width;
-  int height;
-  int text_x0;
-  int text_x1;
-  int x1;
-  int text_y0;
-  int text_y1;
-  int y1;
-  bool rc;
+    const char *const prompt3 = "Press [Y] to confirm, [N] to cancel...";
+    size_t maxlen;
+    double n_lines_of_text;
+    int width;
+    int height;
+    int text_x0;
+    int text_x1;
+    int x1;
+    int text_y0;
+    int text_y1;
+    int y1;
+    bool rc;
 
-  maxlen = strlen (prompt3);
-  if (strlen (prompt1) > maxlen)
-    maxlen = strlen (prompt1);
-  if (prompt2 != NULL && strlen (prompt2) > maxlen)
-    maxlen = strlen (prompt2);
-  n_lines_of_text = (prompt2 == NULL ? 2.5 : 3.5);
-  width = 2 * BOX_BORDER + 2 * WIDE_HSPACING + maxlen * FONTW;
-  height = 2 * BOX_BORDER + 2 * WIDE_VSPACING + (int) (n_lines_of_text * FONTH);
-  if (x0 < 0)
-    x0 = (ScrMaxX - width) / 2;
-  if (y0 < 0)
-    y0 = (ScrMaxY - height) / 2;
-  text_x0 = x0 + BOX_BORDER + WIDE_HSPACING;
-  text_x1 = text_x0 + maxlen * FONTW - 1;
-  x1      = text_x1 + WIDE_HSPACING + BOX_BORDER;
-  text_y0 = y0 + BOX_BORDER + WIDE_VSPACING;
-  text_y1 = text_y0 + (int) (n_lines_of_text * FONTH) - 1;
-  y1      = text_y1 + WIDE_HSPACING + BOX_BORDER;
-  for (bool first_time = true; ; first_time = false)
-  {
-    if (first_time || is.key == YE_EXPOSE)
+    maxlen = strlen (prompt3);
+
+    if (strlen (prompt1) > maxlen)
+        maxlen = strlen (prompt1);
+    if (prompt2 != NULL && strlen (prompt2) > maxlen)
+        maxlen = strlen (prompt2);
+
+    n_lines_of_text = (prompt2 == NULL ? 2.5 : 3.5);
+    width = 2 * BOX_BORDER + 2 * WIDE_HSPACING + maxlen * FONTW;
+    height = 2 * BOX_BORDER + 2 * WIDE_VSPACING + (int) (n_lines_of_text * FONTH);
+
+    if (x0 < 0)
+        x0 = (ScrMaxX - width) / 2;
+    if (y0 < 0)
+        y0 = (ScrMaxY - height) / 2;
+
+    text_x0 = x0 + BOX_BORDER + WIDE_HSPACING;
+    text_x1 = text_x0 + maxlen * FONTW - 1;
+    x1      = text_x1 + WIDE_HSPACING + BOX_BORDER;
+    text_y0 = y0 + BOX_BORDER + WIDE_VSPACING;
+    text_y1 = text_y0 + (int) (n_lines_of_text * FONTH) - 1;
+    y1      = text_y1 + WIDE_HSPACING + BOX_BORDER;
+
+    for (bool first_time = true; ; first_time = false)
     {
-      DrawScreenBox3D (x0, y0, x1, y1);
-      set_colour (WHITE);
-      DrawScreenText (text_x0, text_y0, prompt1);
-      if (prompt2 != NULL)
-	DrawScreenText (text_x0, text_y0 + FONTH, prompt2);
-      set_colour (WINTITLE);
-      DrawScreenText (text_x0, text_y1 - FONTH - 1, prompt3);
+        if (first_time || is.key == YE_EXPOSE)
+        {
+            DrawScreenBox3D (x0, y0, x1, y1);
+            set_colour (WHITE);
+            DrawScreenText (text_x0, text_y0, prompt1);
+
+            if (prompt2 != NULL)
+                DrawScreenText (text_x0, text_y0 + FONTH, prompt2);
+
+            set_colour (WINTITLE);
+            DrawScreenText (text_x0, text_y1 - FONTH - 1, prompt3);
+        }
+        get_input_status ();
+        if (is.key == 'y' || is.key == 'Y' || is.key == YK_RETURN)
+        {
+            rc = true;
+            break;
+        }
+        if (is.key == 'n' || is.key == 'N' || is.key == YK_ESC)
+        {
+            rc = false;
+            break;
+        }
     }
-    get_input_status ();
-    if (is.key == 'y' || is.key == 'Y' || is.key == YK_RETURN)
-    {
-      rc = true;
-      break;
-    }
-    if (is.key == 'n' || is.key == 'N' || is.key == YK_ESC)
-    {
-      rc = false;
-      break;
-    }
-  }
-  is.key = 0;  // Shouldn't have to do that but EditorLoop() is broken
-  return rc;
+    is.key = 0;  // Shouldn't have to do that but EditorLoop() is broken
+    return rc;
 }
-
 
 /*
  *	Confirm2 - ask for confirmation, in a smarter fashion
@@ -109,20 +112,19 @@ bool Confirm (int x0, int y0, const char *prompt1, const char *prompt2)
  *	Return zero for "no", non-zero for "yes".
  */
 int Confirm2 (int x0, int y0, confirm_t *confirm_flag,
-   const char *prompt1, const char *prompt2)
+              const char *prompt1, const char *prompt2)
 {
-  int r;
+    int r;
 
-  if (*confirm_flag == YC_YES)
-    return 1;
-  if (*confirm_flag == YC_NO)
-    return 0;
-  r = Confirm (x0, y0, prompt1, prompt2);
-  if (*confirm_flag == YC_ASK_ONCE)
-    *confirm_flag = r ? YC_YES : YC_NO;  // We won't ask again
-  return r;
+    if (*confirm_flag == YC_YES)
+        return 1;
+    if (*confirm_flag == YC_NO)
+        return 0;
+    r = Confirm (x0, y0, prompt1, prompt2);
+    if (*confirm_flag == YC_ASK_ONCE)
+        *confirm_flag = r ? YC_YES : YC_NO;  // We won't ask again
+    return r;
 }
-
 
 /*
  *	Notify - notification dialog box
@@ -132,46 +134,51 @@ int Confirm2 (int x0, int y0, confirm_t *confirm_flag,
  */
 void Notify (int x0, int y0, const char *prompt1, const char *prompt2)
 {
-  const char *const prompt3 = "Press any key to continue...";
-  size_t maxlen;
-  double n_lines_of_text;
-  int width;
-  int height;
-  int text_x0;
-  int text_x1;
-  int x1;
-  int text_y0;
-  int text_y1;
-  int y1;
+    const char *const prompt3 = "Press any key to continue...";
+    size_t maxlen;
+    double n_lines_of_text;
+    int width;
+    int height;
+    int text_x0;
+    int text_x1;
+    int x1;
+    int text_y0;
+    int text_y1;
+    int y1;
 
-  maxlen = strlen (prompt3);
-  if (strlen (prompt1) > maxlen)
-    maxlen = strlen (prompt1);
-  if (prompt2 != NULL && strlen (prompt2) > maxlen)
-    maxlen = strlen (prompt2);
-  n_lines_of_text = (prompt2 == NULL ? 2.5 : 3.5);
-  width = 2 * BOX_BORDER + 2 * WIDE_HSPACING + maxlen * FONTW;
-  height = 2 * BOX_BORDER + 2 * WIDE_VSPACING + (int) (n_lines_of_text * FONTH);
-  if (x0 < 0)
-    x0 = (ScrMaxX - width) / 2;
-  if (y0 < 0)
-    y0 = (ScrMaxY - height) / 2;
-  text_x0 = x0 + BOX_BORDER + WIDE_HSPACING;
-  text_x1 = text_x0 + maxlen * FONTW - 1;
-  x1      = text_x1 + WIDE_HSPACING + BOX_BORDER;
-  text_y0 = y0 + BOX_BORDER + WIDE_VSPACING;
-  text_y1 = text_y0 + (int) (n_lines_of_text * FONTH) - 1;
-  y1      = text_y1 + WIDE_HSPACING + BOX_BORDER;
-  DrawScreenBox3D (x0, y0, x1, y1);
-  set_colour (WHITE);
-  DrawScreenText (text_x0, text_y0, prompt1);
-  if (prompt2 != NULL)
-    DrawScreenText (text_x0, text_y0 + FONTH, prompt2);
-  set_colour (WINTITLE);
-  DrawScreenText (text_x0, text_y1 - FONTH - 1, prompt3);
-  get_key_or_click ();
+    maxlen = strlen (prompt3);
+
+    if (strlen (prompt1) > maxlen)
+        maxlen = strlen (prompt1);
+    if (prompt2 != NULL && strlen (prompt2) > maxlen)
+        maxlen = strlen (prompt2);
+
+    n_lines_of_text = (prompt2 == NULL ? 2.5 : 3.5);
+    width = 2 * BOX_BORDER + 2 * WIDE_HSPACING + maxlen * FONTW;
+    height = 2 * BOX_BORDER + 2 * WIDE_VSPACING + (int) (n_lines_of_text * FONTH);
+
+    if (x0 < 0)
+        x0 = (ScrMaxX - width) / 2;
+    if (y0 < 0)
+        y0 = (ScrMaxY - height) / 2;
+
+    text_x0 = x0 + BOX_BORDER + WIDE_HSPACING;
+    text_x1 = text_x0 + maxlen * FONTW - 1;
+    x1      = text_x1 + WIDE_HSPACING + BOX_BORDER;
+    text_y0 = y0 + BOX_BORDER + WIDE_VSPACING;
+    text_y1 = text_y0 + (int) (n_lines_of_text * FONTH) - 1;
+    y1      = text_y1 + WIDE_HSPACING + BOX_BORDER;
+    DrawScreenBox3D (x0, y0, x1, y1);
+    set_colour (WHITE);
+    DrawScreenText (text_x0, text_y0, prompt1);
+
+    if (prompt2 != NULL)
+        DrawScreenText (text_x0, text_y0 + FONTH, prompt2);
+
+    set_colour (WINTITLE);
+    DrawScreenText (text_x0, text_y1 - FONTH - 1, prompt3);
+    get_key_or_click ();
 }
-
 
 /*
  *	debmes - Display a message in a box only if in debug mode
@@ -183,48 +190,46 @@ void Notify (int x0, int y0, const char *prompt1, const char *prompt2)
  */
 void debmes (const char *fmt, ...)
 {
-  char buf[200];
-  va_list arglist;
+    char buf[200];
+    va_list arglist;
 
-  if (Debug != 1)
-    return;
-  va_start (arglist, fmt);
-  y_vsnprintf (buf, sizeof buf, fmt, arglist);
-  Notify (-1, -1, buf, NULL);
+    if (Debug != 1)
+        return;
+    va_start (arglist, fmt);
+    y_vsnprintf (buf, sizeof buf, fmt, arglist);
+    Notify (-1, -1, buf, NULL);
 }
-
 
 /*
  *	DisplayMessage - clear the screen and display a message
  */
 void DisplayMessage (int x0, int y0, const char *msg, ...)
 {
-  char prompt[120];
-  va_list args;
+    char prompt[120];
+    va_list args;
 
-  va_start (args, msg);
-  y_vsnprintf (prompt, sizeof prompt, msg, args);
-  int width = 2 * BOX_BORDER + 2 * WIDE_HSPACING + FONTW * strlen (prompt);
-  int height = 2 * BOX_BORDER + 2 * WIDE_VSPACING + FONTH;
-  if (x0 < 0)
-    x0 = (ScrMaxX - width) / 2;
-  if (y0 < 0)
-    y0 = (ScrMaxY - height) / 2;
-  DrawScreenBox3D (x0, y0, x0 + width - 1, y0 + height - 1);
-  push_colour (WINFG);
-  DrawScreenText (x0 + BOX_BORDER + WIDE_HSPACING,
+    va_start (args, msg);
+    y_vsnprintf (prompt, sizeof prompt, msg, args);
+    int width = 2 * BOX_BORDER + 2 * WIDE_HSPACING + FONTW * strlen (prompt);
+    int height = 2 * BOX_BORDER + 2 * WIDE_VSPACING + FONTH;
+
+    if (x0 < 0)
+        x0 = (ScrMaxX - width) / 2;
+    if (y0 < 0)
+        y0 = (ScrMaxY - height) / 2;
+
+    DrawScreenBox3D (x0, y0, x0 + width - 1, y0 + height - 1);
+    push_colour (WINFG);
+    DrawScreenText (x0 + BOX_BORDER + WIDE_HSPACING,
     y0 + BOX_BORDER + WIDE_VSPACING, prompt);
-  pop_colour ();
-  XFlush (dpy);
+    pop_colour ();
+    XFlush (dpy);
 }
-
 
 /*
  *	NotImplemented - make the user angry...
  */
 void NotImplemented (void)
 {
-  Notify (-1, -1, "This function is not implemented... Yet!", NULL);
+    Notify (-1, -1, "This function is not implemented... Yet!", NULL);
 }
-
-
--- src/dialog.h
+++ src/dialog.h
@@ -4,13 +4,10 @@
  *	AYM 1998-11-30
  */
 
-
 bool Confirm (int, int, const char *, const char *);
 int Confirm2 (int x0, int y0, confirm_t *confirm_flag,
-   const char *prompt1, const char *prompt2);
+              const char *prompt1, const char *prompt2);
 void Notify (int, int, const char *, const char *);
 void debmes (const char *fmt, ...);
 void DisplayMessage (int, int, const char *, ...);
 void NotImplemented (void);
-
-

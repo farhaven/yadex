53875d98d250a2afe552ce695feffde39977e2da
Gregor Best <gbe@intepi.net>
2009-12-10 23:37:39 +0100

    code cleanup
    Signed-off-by: Gregor Best <gbe@intepi.net>

--- src/checks.h
+++ src/checks.h
@@ -3,12 +3,9 @@
  *	AYM 1998-12-02
  */
 
-
 void CheckLevel (int x0, int y0);
 void Statistics ();
 void CheckSectors ();
 void CheckCrossReferences ();
 void CheckTextures ();
 void CheckTextureNames ();
-
-

3aef1978ccf4a9bc76565190c9cc65ddf7e90836
Gregor Best <gbe@intepi.net>
2009-12-10 23:17:12 +0100

    code cleanup
    Signed-off-by: Gregor Best <gbe@intepi.net>

--- src/cfgfile.h
+++ src/cfgfile.h
@@ -3,12 +3,9 @@
  *	AYM 1998-09-30
  */
 
-
 int parse_config_file_default ();
 int parse_config_file_user (const char *name);
 int  parse_command_line_options (int argc, const char *const *argv, int pass);
 void dump_parameters (FILE *fd);
 void dump_command_line_options (FILE *fd);
 extern const char config_file_magic[];
-
-

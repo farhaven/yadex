dfae6e17b5cb4e943c17c6676590b56622c14bae
Gregor Best <gbe@intepi.net>
2009-12-10 23:16:51 +0100

    indention fix
    Signed-off-by: Gregor Best <gbe@intepi.net>

--- src/bitvec.cc
+++ src/bitvec.cc
@@ -3,11 +3,7 @@
  *	AYM 1999-08-15
  */
 
-
 #include "yadex.h"
 #include "bitvec.h"
 
-
 const char _bitvec_msg1[] = "bitvec::frob: invalid op = %d";
-
-
--- src/bitvec.h
+++ src/bitvec.h
@@ -4,7 +4,6 @@
  *	AYM 1998-11-22
  */
 
-
 /*
 This file is part of Yadex.
 
@@ -31,80 +30,75 @@ Place, Suite 330, Boston, MA 02111-1307, USA.
 #ifndef YH_BITVEC  /* DO NOT INSERT ANYTHING BEFORE THIS LINE */
 #define YH_BITVEC
 
-
 #include <limits.h>
 
 #include "yerror.h"
 #include "ymemory.h"
 
-
 typedef enum
-   {
+{
    BV_SET    = 's',
    BV_CLEAR  = 'c',
    BV_TOGGLE = 't'
-   } bitvec_op_t;
-
+} bitvec_op_t;
 
 extern const char _bitvec_msg1[];
 
-
 class bitvec_c
-   {
-   public :
-      bitvec_c (size_t n_elements)
-         { 
-	 a = (char *) GetMemory (n_elements / CHAR_BIT + 1);
- 	 memset (a, 0, n_elements / CHAR_BIT + 1);
-         _n_elements = n_elements;
-         }
+{
+    public :
+        bitvec_c (size_t n_elements)
+        { 
+            a = (char *) GetMemory (n_elements / CHAR_BIT + 1);
+            memset (a, 0, n_elements / CHAR_BIT + 1);
+            _n_elements = n_elements;
+        }
 
-      ~bitvec_c ()
-         {
-         FreeMemory (a);
-         }
+        ~bitvec_c ()
+        {
+            FreeMemory (a);
+        }
 
-      size_t nelements () const  // Return the number of elements
-         {
-         return _n_elements;
-         }
+        size_t nelements () const  // Return the number of elements
+        {
+            return _n_elements;
+        }
 
-      int get (size_t n) const  // Get bit <n>
-         {
-         return a[n/CHAR_BIT] & (1 << (n % CHAR_BIT));
-         }
+        int get (size_t n) const  // Get bit <n>
+        {
+            return a[n/CHAR_BIT] & (1 << (n % CHAR_BIT));
+        }
 
-      void set (size_t n)  // Set bit <n> to 1
-         {
-         a[n/CHAR_BIT] |= 1 << (n % CHAR_BIT);
-         }
+        void set (size_t n)  // Set bit <n> to 1
+        {
+            a[n/CHAR_BIT] |= 1 << (n % CHAR_BIT);
+        }
 
-      void unset (size_t n)  // Set bit <n> to 0
-         {
-         a[n/CHAR_BIT] &= ~(1 << (n % CHAR_BIT));
-         }
+        void unset (size_t n)  // Set bit <n> to 0
+        {
+            a[n/CHAR_BIT] &= ~(1 << (n % CHAR_BIT));
+        }
 
-      void toggle (size_t n)  // Toggle bit <n>
- 	 {
-	 a[n/CHAR_BIT] ^= (1 << (n % CHAR_BIT));
-	 }
+        void toggle (size_t n)  // Toggle bit <n>
+        {
+            a[n/CHAR_BIT] ^= (1 << (n % CHAR_BIT));
+        }
 
-      void frob (size_t n, bitvec_op_t op)  // Set, unset or toggle bit <n>
-         {
-	 if (op == BV_SET)
-	    set (n);
-	 else if (op == BV_CLEAR)
-	    unset (n);
-	 else if (op == BV_TOGGLE)
-	    toggle (n);
-	 else
-	    nf_bug (_bitvec_msg1, (int) op);
-         }
+        void frob (size_t n, bitvec_op_t op)  // Set, unset or toggle bit <n>
+        {
+            if (op == BV_SET)
+                set (n);
+            else if (op == BV_CLEAR)
+                unset (n);
+            else if (op == BV_TOGGLE)
+                toggle (n);
+            else
+                nf_bug (_bitvec_msg1, (int) op);
+        }
 
-   private :
-      size_t _n_elements;
-      char *a;
-   };
-
+    private :
+        size_t _n_elements;
+        char *a;
+};
 
 #endif  /* DO NOT ADD ANYTHING AFTER THIS LINE */

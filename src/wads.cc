d8d83118d157a8dfa4b9695b2dd5b256c5fbbee2
Gregor Best <gbe@unobtanium.de>
2016-05-18 22:06:42 +0200

    Reflow

--- src/wads.cc
+++ src/wads.cc
@@ -42,14 +42,14 @@ Serial_num master_dir_serial;		// The revision# thereof
  *
  *	Return 0 on success, non-zero on failure.
  */
-int file_read_int16_t (FILE *fp, int16_t *buf, long count)
-{
-  while (count-- > 0) {
-	  uint8_t c1 = getc(fp);
-	  uint8_t c2 = getc(fp);
-    *buf = c1 | (c2 << 8);
-  }
-  return feof (fp) || ferror (fp);
+int
+file_read_int16_t (FILE *fp, int16_t *buf, long count) {
+	while (count-- > 0) {
+		uint8_t c1 = getc(fp);
+		uint8_t c2 = getc(fp);
+		*buf = c1 | (c2 << 8);
+	}
+	return feof (fp) || ferror (fp);
 }
 
 
@@ -58,8 +58,8 @@ int file_read_int16_t (FILE *fp, int16_t *buf, long count)
  *
  *	Return 0 on success, non-zero on failure.
  */
-int file_read_int32_t (FILE *fp, int32_t *buf, long count)
-{
+int
+file_read_int32_t (FILE *fp, int32_t *buf, long count) {
 	while (count-- > 0) {
 		uint8_t c[4];
 		*buf = 0;
@@ -78,26 +78,25 @@ int file_read_int32_t (FILE *fp, int32_t *buf, long count)
  *
  *	Return the number of bytes read.
  */
-long file_read_vbytes (FILE *fp, void *buf, long count)
-{
-  long bytes_read_total;
-  size_t bytes_read;
-  size_t bytes_to_read;
+long
+file_read_vbytes (FILE *fp, void *buf, long count) {
+	long bytes_read_total;
+	size_t bytes_read;
+	size_t bytes_to_read;
 
-  bytes_read_total = 0;
-  bytes_to_read    = 0x8000;
-  while (count > 0)
-  {
-    if (count <= 0x8000)
-      bytes_to_read = (size_t) count;
-    bytes_read = fread (buf, 1, bytes_to_read, fp);
-    bytes_read_total += bytes_read;
-    if (bytes_read != bytes_to_read)
-      break;
-    buf = (char *) buf + bytes_read;
-    count -= bytes_read;
-  }
-  return bytes_read_total;
+	bytes_read_total = 0;
+	bytes_to_read    = 0x8000;
+	while (count > 0) {
+		if (count <= 0x8000)
+			bytes_to_read = (size_t) count;
+		bytes_read = fread (buf, 1, bytes_to_read, fp);
+		bytes_read_total += bytes_read;
+		if (bytes_read != bytes_to_read)
+			break;
+		buf = (char *) buf + bytes_read;
+		count -= bytes_read;
+	}
+	return bytes_read_total;
 }
 
 
@@ -106,9 +105,9 @@ long file_read_vbytes (FILE *fp, void *buf, long count)
  *
  *	Return 0 on success, non-zero on failure.
  */
-int file_read_bytes (FILE *fp, void *buf, long count)
-{
-  return file_read_vbytes (fp, buf, count) != count;
+int
+file_read_bytes (FILE *fp, void *buf, long count) {
+	return file_read_vbytes (fp, buf, count) != count;
 }
 
 
@@ -117,10 +116,10 @@ int file_read_bytes (FILE *fp, void *buf, long count)
  *
  *	Does no error checking.
  */
-void file_write_int16_t (FILE *fd, int16_t buf)
-{
-  putc (       buf & 0xff, fd);
-  putc ((buf >> 8) & 0xff, fd);
+void
+file_write_int16_t (FILE *fd, int16_t buf) {
+	putc (       buf & 0xff, fd);
+	putc ((buf >> 8) & 0xff, fd);
 }
 
 
@@ -129,17 +128,16 @@ void file_write_int16_t (FILE *fd, int16_t buf)
  *
  *	Does no error checking.
  */
-void file_write_int32_t (FILE *fd, int32_t buf, long count)
-{
-  /* It would probably be more efficient to swap bytes in-core
-     and write the whole int32_t at once. */
-  while (count-- > 0)
-  {
-    putc (        buf & 0xff, fd);
-    putc ((buf >>  8) & 0xff, fd);
-    putc ((buf >> 16) & 0xff, fd);
-    putc ((buf >> 24) & 0xff, fd);
-  }
+void
+file_write_int32_t (FILE *fd, int32_t buf, long count) {
+	/* It would probably be more efficient to swap bytes in-core
+		and write the whole int32_t at once. */
+	while (count-- > 0) {
+		putc (        buf & 0xff, fd);
+		putc ((buf >>  8) & 0xff, fd);
+		putc ((buf >> 16) & 0xff, fd);
+		putc ((buf >> 24) & 0xff, fd);
+	}
 }
 
 
@@ -160,15 +158,14 @@ void file_write_int32_t (FILE *fd, int32_t buf, long count)
  *
  *	Does no error checking.
  */
-void file_write_name (FILE *fd, const char *name)
-{
-  const unsigned char *const p0 = (const unsigned char *) name;
-  const unsigned char *p = p0;  // "unsigned" for toupper()'s sake
+void file_write_name (FILE *fd, const char *name) {
+	const unsigned char *const p0 = (const unsigned char *) name;
+	const unsigned char *p = p0;  // "unsigned" for toupper()'s sake
 
-  for (; p - p0 < (ptrdiff_t) WAD_NAME && *p; p++)
-    putc (toupper (*p), fd);
-  for (; p - p0 < (ptrdiff_t) WAD_NAME; p++)
-    putc ('\0', fd);
+	for (; p - p0 < (ptrdiff_t) WAD_NAME && *p; p++)
+		putc (toupper (*p), fd);
+	for (; p - p0 < (ptrdiff_t) WAD_NAME; p++)
+		putc ('\0', fd);
 }
 
 
@@ -176,31 +173,29 @@ void file_write_name (FILE *fd, const char *name)
    find an entry in the master directory
 */
 
-MDirPtr FindMasterDir (MDirPtr from, const char *name)
-{
-  while (from)
-  {
-    if (! y_strnicmp (from->dir.name, name, WAD_NAME))
-      break;
-    from = from->next;
-  }
-  return from;
+MDirPtr
+FindMasterDir (MDirPtr from, const char *name) {
+	while (from) {
+		if (! y_strnicmp (from->dir.name, name, WAD_NAME))
+			break;
+		from = from->next;
+	}
+	return from;
 }
 
 
 /*
  *	Find an entry in the master directory
  */
-MDirPtr FindMasterDir (MDirPtr from, const char *name1, const char *name2)
-{
-  while (from)
-    {
-    if (! y_strnicmp (from->dir.name, name1, WAD_NAME)
-     || ! y_strnicmp (from->dir.name, name2, WAD_NAME))
-      break;
-    from = from->next;
-    }
-  return from;
+MDirPtr
+FindMasterDir (MDirPtr from, const char *name1, const char *name2) {
+	while (from) {
+		if (! y_strnicmp (from->dir.name, name1, WAD_NAME)
+				|| ! y_strnicmp (from->dir.name, name2, WAD_NAME))
+			break;
+		from = from->next;
+	}
+	return from;
 }
 
 
@@ -208,19 +203,18 @@ MDirPtr FindMasterDir (MDirPtr from, const char *name1, const char *name2)
    output bytes to a binary file with error checking
 */
 
-void WriteBytes (FILE *file, const void *buf, long size)
-{
-  if (! Registered)
-    return;
-  while (size > 0x8000)
-  {
-    if (fwrite (buf, 1, 0x8000, file) != 0x8000)
-      fatal_error ("error writing to file");
-    buf = (const char *) buf + 0x8000;
-    size -= 0x8000;
-  }
-  if (fwrite (buf, 1, size, file) != (size_t) size)
-    fatal_error ("error writing to file");
+void
+WriteBytes (FILE *file, const void *buf, long size) {
+	if (!Registered)
+		return;
+	while (size > 0x8000) {
+		if (fwrite (buf, 1, 0x8000, file) != 0x8000)
+			fatal_error ("error writing to file");
+		buf = (const char *) buf + 0x8000;
+		size -= 0x8000;
+	}
+	if (fwrite (buf, 1, size, file) != (size_t) size)
+		fatal_error ("error writing to file");
 }
 
 
@@ -235,40 +229,37 @@ void WriteBytes (FILE *file, const void *buf, long size)
  *	source file, 2 if there was a write error on destination
  *	file.
  */
-int copy_bytes (FILE *dest, FILE *source, size_t size)
-{
-  int          rc      = 0;
-  void        *data    = 0;
-  const size_t chunksz = 0x4000;
+int
+copy_bytes (FILE *dest, FILE *source, size_t size) {
+	int          rc      = 0;
+	void        *data    = 0;
+	const size_t chunksz = 0x4000;
 
-  data = malloc(chunksz + 2);
-  while (size > chunksz)
-  {
-    if (fread (data, 1, chunksz, source) != chunksz)
-    {
-      rc = 1;
-      goto byebye;
-    }
-    if (fwrite (data, 1, chunksz, dest) != chunksz)
-    {
-      rc = 2;
-      goto byebye;
-    }
-    size -= chunksz;
-  }
-  if (fread (data, 1, size, source) != (size_t) size)
-  {
-    rc = 1;
-    goto byebye;
-  }
-  if (fwrite (data, 1, size, dest) != (size_t) size)
-  {
-    rc = 2;
-    goto byebye;
-  }
+	data = malloc(chunksz + 2);
+	while (size > chunksz) {
+		if (fread (data, 1, chunksz, source) != chunksz) {
+			rc = 1;
+			goto byebye;
+		}
+
+		if (fwrite (data, 1, chunksz, dest) != chunksz) {
+			rc = 2;
+			goto byebye;
+		}
+		size -= chunksz;
+	}
+
+	if (fread (data, 1, size, source) != (size_t) size) {
+		rc = 1;
+		goto byebye;
+	}
+
+	if (fwrite (data, 1, size, dest) != (size_t) size) {
+		rc = 2;
+		goto byebye;
+	}
 
 byebye:
-   free(data);
-  return rc;
+	free(data);
+	return rc;
 }
-
